ab8a5d25327e47c95afa0e6aa1fca6e00cc9568f
miray <mirayayerdem@gmail.com>
2020-11-26 09:47:35 +0300

    MyFrame Class has been modified

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,12 +1,18 @@
+/**
+ * a class creates a frame
+ * author: Miray Ayerdem
+ * version : 26/11/2020
+ */
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
-    MyFrame(int num)
+    //Constructor
+    MyFrame(int num) //param takes the dimension from user
     {
         MyPanel panel = new MyPanel(num);
-        this.getContentPane().add(panel);
+        this.getContentPane().add(panel); //panel is adding to frame
         this.pack();
-        setSize(300,300);
+        setSize( 100 * num,100 * num);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
